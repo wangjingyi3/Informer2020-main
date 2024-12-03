6b472f046dade7b9a5019ba74fa26a4483122bea
wangjingyi3 <79513154+wangjingyi3@users.noreply.github.com>
2024-12-03 14:17:47 +0800

    Update data_loader.py

--- data/data_loader.py
+++ data/data_loader.py
@@ -241,7 +241,7 @@ class Dataset_Custom(Dataset):
         df_raw = df_raw[['date']+cols+[self.target]]
 
         num_train = int(len(df_raw)*0.7)
-        num_test = int(len(df_raw)*0.1)
+        num_test = int(len(df_raw)*0.2)
         num_vali = len(df_raw) - num_train - num_test
         border1s = [0, num_train-self.seq_len,
                     len(df_raw)-num_test-self.seq_len]
